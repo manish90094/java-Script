5fce0f21c6d9c73196431ea1677d1028ca4a20b3
manish90094 <140897993+manish90094@users.noreply.github.com>
2024-09-21 14:49:49 +0000

    data type

---
+++ 01_basic/Data_type.js
@@ -0,0 +1,42 @@
+
+"use Strict" // treat all js code in newer version
+
+//alert(2+2) // we are using nodejs , not browser
+
+
+
+let name = "manish"
+
+let age = 82
+
+let isLoggedIn = true
+
+/*
+
+number range  2 to power 53
+bigint -----> bahot bada number
+
+string = ""
+
+boolean
+
+null => standalone value
+
+undefined =>
+
+    symbol => unique
+
+    object
+
+
+    */
+
+
+
+
+console.log(typeof age)
+
+
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof "manish")
