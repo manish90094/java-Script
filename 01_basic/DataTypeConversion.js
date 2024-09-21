1f5bda4954d602b3ce15fdaf58c95289bae776e3
manish90094 <140897993+manish90094@users.noreply.github.com>
2024-09-21 15:46:29 +0000

    DataTypeConversion

---
+++ 01_basic/DataTypeConversion.js
@@ -0,0 +1,49 @@
+
+let score = "undefined"
+
+//console.log(typeof score)
+
+//console.log(typeof (score))
+
+
+let valueInNumber = Number(score)
+
+//console.log(typeof (valueInNumber))
+//console.log(valueInNumber)
+
+
+/*
+"525"  => 525
+
+"12jsa" => NaN (note a number)
+
+true => 1 , false => 0
+
+*/
+
+let isLoggedIn = 0
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanLoggedIn)
+
+/*
+"manish" => true
+
+"87" =>true
+ 1 => true
+ 0 => false
+
+*/
+
+
+let someNumber = 2321
+let letstringNumber = String(someNumber)
+
+console.log(letstringNumber)
+console.log(typeof letstringNumber)
+
+
+
+
+
