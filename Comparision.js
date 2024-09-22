20ab9245cf2872b4c68871615b71e3fa4bc971b6
manish90094 <140897993+manish90094@users.noreply.github.com>
2024-09-22 13:45:47 +0000

    Comparision data tpe

---
+++ Comparision.js
@@ -0,0 +1,29 @@
+
+// console.log(2 > 1)
+// console.log(2 != 1)
+// console.log(2 < 1)
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 > 1)
+
+ /*******************Comparision defrent data type tab problem aati h***************************/
+
+
+ console.log("2" == 1)
+ console.log("2" ===  1)
+ console.log(null < 0)
+ console.log(null  > 0)
+ console.log(null == 0)
+ 
+
+ // comparision ke liye (=== use karenge kyu ki ye value ke sath data type bhi compair karta h)
+ 
+
+
+
+
+
+
+
+
+
