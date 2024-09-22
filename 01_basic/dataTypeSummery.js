cd6dfc3fc2c661b9d6657837d1b9b974fc493123
manish90094 <140897993+manish90094@users.noreply.github.com>
2024-09-22 15:17:43 +0000

    data type summery

---
+++ 01_basic/dataTypeSummery.js
@@ -0,0 +1,44 @@
+// primitive data type (call by value)
+
+// string  , number , boolean , null , undefined , symbol , bigInt
+
+// const score = 612876 // number
+
+// const isLoggedIn = false // boolean
+
+// const isString  = "manish" // string
+
+// const outSidetem = null
+
+// let userEmail ; // undefine
+
+// const Bignumber = 9819809180380188901n
+
+const id = Symbol("123")  // symbol har bar alag alag (unic value deta h)
+
+const anotherId = Symbol("123")
+
+console.log(id === anotherId)
+
+console.log("kjajsj")
+
+
+// Refrence     (none primitive )
+
+// array , obj , function , 
+
+
+const frute = ["apple" , "banana" ];
+
+// key value pair me value dalte h
+const userDetail = {
+     name : "manish",
+     age : 72,
+     city : "singrauli"
+}
+
+const myFunction = function(){
+
+    console.log("Hello world")
+    
+}
