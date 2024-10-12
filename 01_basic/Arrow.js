7200fca6e5b9e6aaca03ac2cf72125081e35107b
manish90094 <140897993+manish90094@users.noreply.github.com>
2024-10-12 14:02:08 +0000

    Arrow function

---
+++ 01_basic/Arrow.js
@@ -0,0 +1,11 @@
+
+const user = {
+    user_name : "manish",
+    price : 999,
+
+    Welcome : function(){
+        console.log(`${this.user_name} , welcome to weside`)
+    }
+}
+
+console.log(user)
